93f9fe1b4b738e0098c6d38e61e9ecdc66dd9ece
Nathan James <scatter.eos@gmail.com>
2018-08-10 15:24:52 +0300

    macos copy paste

--- electron.js
+++ electron.js
@@ -65,6 +65,26 @@ app.on('ready', function () {
 
     setupMenu();
 
+    if (process.platform === 'darwin') {
+      // Create our menu entries so that we can use MAC shortcuts
+      Menu.setApplicationMenu(Menu.buildFromTemplate([
+        {
+          label: 'Edit',
+          submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteandmatchstyle' },
+            { role: 'delete' },
+            { role: 'selectall' }
+          ]
+        }
+      ]));
+    }
+
 
 
 });
