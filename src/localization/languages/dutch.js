69df24a3c43ea6e508d0f860874d78730c12c204
Tom Veldman <37376120+progBorg@users.noreply.github.com>
2018-10-19 15:35:40 +0200

    Create dutch.js

---
+++ src/localization/languages/dutch.js
@@ -0,0 +1,10 @@
+import * as KEYS from '../keys';
+
+export default {
+    [KEYS.MAINMENU_Identities]:'Identiteiten',
+    [KEYS.MAINMENU_Keys]:'Sleutelparen',
+    [KEYS.MAINMENU_Permissions]:'Bevoegdheid',
+    [KEYS.MAINMENU_History]:'Geschiedenis',
+    [KEYS.MAINMENU_Lock]:'Sluit',
+
+}
