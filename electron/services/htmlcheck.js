e05323d6fbc7afd4f379f7eb151e48ca719be785
nsjames <skyandbeyond@gmail.com>
2019-10-08 02:50:09 +0300

    better hash and signature checking

--- electron/services/htmlcheck.js
+++ electron/services/htmlcheck.js
@@ -1,16 +1,18 @@
 const {dialog} = require('electron');
 const {getDefaultPath, saveFile, openFile, existsOrMkdir, exists} = require('./files');
 const createHash = require('create-hash');
-const {GET} = require('./embedapi');
+const {POST} = require('@walletpack/core/services/apis/BackendApiService');
 const path = require('path');
+const ecc = require('eosjs-ecc');
 
 
 const HOST = process.env.WEB_HOST;
+const PROOF_KEYS = process.env.PROOF_KEYS.split(',');
 let ETAGS = {};
 
 const hash = x => createHash('sha256').update(x).digest('hex')
 
-const applyETAG = async (filename, tag) => {
+const cacheETAG = async (filename, tag) => {
 	ETAGS[filename] = tag;
 	existsOrMkdir(getDefaultPath());
 	await saveFile(getDefaultPath(), 'etags.json', JSON.stringify(ETAGS));
@@ -18,7 +20,6 @@ const applyETAG = async (filename, tag) => {
 }
 
 const getSource = (filename, method = "GET") => {
-	console.log('getting source', filename);
 	return fetch(`${HOST}/${filename}`, {method, cache:"no-cache"}).then(async x => {
 		return {etag:x.headers.get('etag'), file:await x.text()};
 	}).catch(err => {
@@ -41,7 +42,19 @@ const saveSource = (filename, file) => {
 };
 
 
-
+const checkSignature = (hashed, signed) => {
+	console.log('checking signature', signed, hashed)
+	let proven = false;
+	for(let i = 0; i < PROOF_KEYS.length; i++){
+		try {
+			if(ecc.recoverHash(signed, hashed) === PROOF_KEYS[i]) {
+				proven = true;
+				break;
+			}
+		} catch(e){}
+	}
+	return proven;
+}
 
 
 
@@ -52,13 +65,13 @@ class WebHashChecker {
 	static async check(window){
 		const isTesting = process.env.WEB_HOST.indexOf('http://localhost:') === 0;
 
-		// Don't do checks when running on localhost
-		// if(process.env.WEB_HOST.indexOf('http://localhost:') === 0) return true;
 
-
-		const hashes = await GET(`hashes`).catch(() => null);
-		if(!hashes) return dialog.showErrorBox(ERR_TITLE, API_ERR);
-		const filesList = Object.keys(hashes);
+		// Simply gets a list of files that need verification.
+		// If this list is spoofed, Scatter simply won't run as it will be missing files.
+		// And in the case of adding files to the list, those files will never be executed
+		// as there would be nothing to execute them since normal files are hash verified.
+		const filesList = await fetch(`${HOST}/hashes/`).then(x => x.json()).then(x => x.map(y => y.name.replace('.hash', ''))).catch(() => null);
+		if(!filesList) return dialog.showErrorBox(ERR_TITLE, API_ERR);
 
 		const etagsFile = await openFile(path.join(getDefaultPath(), 'etags.json')).catch(() => null);
 		if(etagsFile) ETAGS = JSON.parse(etagsFile);
@@ -67,25 +80,56 @@ class WebHashChecker {
 
 		const checkFileHash = async (filename) => {
 			if(error) return false;
+
+
+			// Sources are fetched on a round-robin basis, so each file is gotten
+			// from a different server, making the attack surface as large as our server count.
 			const result = await getSource(filename).catch(() => null);
 			if(!result || !result.file.length) return error = WEB_APP_ERR;
 
-			if(isTesting || hash(result.file) === hashes[filename]){
-				await applyETAG(filename, result.etag);
+			// Hashes and signatures are also fetched on a round-robin basis, so each hash/sig for a file is gotten
+			// from a different server than the one the file was fetched from.
+			const hashsig = await getSource(`hashes/${filename}.hash`).then(x => x.file.trim()).catch(() => null);
+			if(!hashsig) return error = API_ERR;
 
-				// Applying absolute URLs to relative static assets
-				result.file = result.file.replace(/static\//g, "https://embed.get-scatter.com/static/");
+			const [hashed, signed] = hashsig.split('|').map(x => x.trim());
+			const fileVerified = () =>
+				hash(result.file) === hashed &&
+				checkSignature(hashed, signed);
+
+			if(isTesting || fileVerified()){
+				await cacheETAG(filename, result.etag);
+
+				// Applying absolute URLs to relative static assets.
+				// Note: These files won't be available if Embed is down, however they are all purely aesthetic.
+				// This saves the user from download another 3+mb of data which will be static anyway.
 				result.file = result.file.replace(/static\//g, "https://embed.get-scatter.com/static/");
 
-				// Saving the source locally.
+				// Saving the source locally for quicker use and fallback for later hash verification failures.
+				// This makes it so the user's local Scatter can never "not work" just because the online Embed is down.
 				saveSource(filename, result.file);
+
 				return true;
 			}
 
+			// Alerts about bad hashes are sent to a completely separate API which runs under different
+			// security measures and is detached from the entire Embed system.
+			await POST(`bad_hash`, {
+				filename,
+				fileHash:hash(result.file),
+				preHash:hashed,
+				signed,
+			});
 
 			return false;
 		};
 
+		// If an ETAG already exists on the user's local machine then
+		// Scatter won't try to refresh the Embed file.
+		// This is completely to rely on since even if a malicious server spoofs the ETAG
+		// Scatter will simply not download their malicious version of the file as it will
+		// use the one locally stored on the user's machine and not the one with the spoofed
+		// ETAG.
 		const checkEtag = async (filename) => {
 			if(error) return false;
 			if(ETAGS.hasOwnProperty(filename) && ETAGS[filename]){
