99a05c4d8191512615a336f60e56aafdcc373134
nsjames <skyandbeyond@gmail.com>
2019-12-01 15:17:28 +0200

    dynamic staging env

--- electron/services/getHost.js
+++ electron/services/getHost.js
@@ -1,7 +1,10 @@
-const {getSimpleMode} = require('./storage');
+const {getSimpleMode, getGeneralSetting} = require('./storage');
 // getHost
 module.exports = () => {
 	if(process.env.LOCAL_TESTING) return process.env.LOCAL_TESTING;
-	return getSimpleMode() ? process.env.WEB_HOST_SIMPLE_MODE : process.env.WEB_HOST;
 
+	let host = getSimpleMode() ? process.env.WEB_HOST_SIMPLE_MODE : process.env.WEB_HOST;
+	if(getGeneralSetting('testingMode')) host = 'staging.'+host;
+
+	return `https://${host}`;
 };
